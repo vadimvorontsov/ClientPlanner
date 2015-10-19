21319da9e6371dde578d4c8fe92434360c6c2c8a
Stewie <temik_gaia@mail.ru>
2015-10-19 22:15:00 +0400

    19/10 20:15 color procedure

---
+++ app/src/main/java/ru/anroidapp/plannerwork/intface_procedure/ProcedureHeaderAdapter.java
@@ -0,0 +1,207 @@
+package ru.anroidapp.plannerwork.intface_procedure;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.smena.clientbase.procedures.Procedures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import ru.anroidapp.plannerwork.CircularImageView;
+import ru.anroidapp.plannerwork.R;
+
+public class ProcedureHeaderAdapter extends BaseAdapter implements AbsListView.OnScrollListener, IPinnedHeaderProcedure, Filterable {
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_SECTION = 1;
+    private static final int TYPE_MAX_COUNT = TYPE_SECTION + 1;
+
+    LayoutInflater mLayoutInflater;
+    int mCurrentSectionPosition = 0, mNextSectionPosition = 0;
+
+    // array list to store section positions
+    ArrayList<Integer> mListSectionPos;
+
+    // array list to store list view data
+    List<String> mListItems;
+
+    // context object
+    Context mContext;
+
+    int iColor = 0;
+    int testColor = 0;
+
+    ArrayList<Integer> mColorProcedures;
+
+    int[] arColorProcedures ={R.mipmap.ic_launcher_blue,R.mipmap.ic_launcher_orange, R.mipmap.ic_launcher_green, R.mipmap.ic_launcher_red};
+
+
+    public ProcedureHeaderAdapter(Context context, List<String> listItems,
+                               ArrayList<Integer> listSectionPos, ArrayList<Integer> colorProcedures) {
+        this.mContext = context;
+        this.mListItems = listItems;
+        this.mListSectionPos = listSectionPos;
+        this.mColorProcedures = colorProcedures;
+
+        //mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        return mListItems.size();
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return !mListSectionPos.contains(position);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return TYPE_MAX_COUNT;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mListSectionPos.contains(position) ? TYPE_SECTION : TYPE_ITEM;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mListItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return mListItems.get(position).hashCode();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            int type = getItemViewType(position);
+
+            switch (type) {
+                case TYPE_ITEM:
+                    convertView = mLayoutInflater.inflate(R.layout.procedure_row_view, null);
+                    holder.textView = (TextView) convertView.findViewById(R.id.row_title);
+                    convertView.setTag(holder);
+                    String name = holder.textView.getText().toString();
+                    getColorProceduresName(name);
+                    ImageView icon = (ImageView) convertView.findViewById(R.id.contact_circle);
+                    icon.setImageResource(arColorProcedures[testColor]);
+                    iColor += 1;
+                    if (iColor == mListItems.size() - mListSectionPos.size())
+                        iColor = 0;
+
+//                    holder.contactPhoto = (CircularImageView) convertView.findViewById(R.id.contact_circle);
+//                    Drawable drawable = mContext.getDrawable(R.drawable.header);
+//                    holder.contactPhoto.setImageDrawable(drawable);
+                    break;
+                case TYPE_SECTION:
+                    convertView = mLayoutInflater.inflate(R.layout.contact_section_row_view, null);
+                    holder.textView = (TextView) convertView.findViewById(R.id.row_title);
+                    convertView.setTag(holder);
+                    break;
+            }
+
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.textView.setText(mListItems.get(position).toString());
+
+        return convertView;
+    }
+
+    @Override
+    public int getPinnedHeaderState(int position) {
+        // hide pinned header when items count is zero OR position is less than
+        // zero OR
+        // there is already a header in list view
+        if (getCount() == 0 || position < 0 || mListSectionPos.indexOf(position) != -1) {
+            return PINNED_HEADER_GONE;
+        }
+
+        // the header should get pushed up if the top item shown
+        // is the last item in a section for a particular letter.
+        mCurrentSectionPosition = getCurrentSectionPosition(position);
+        mNextSectionPosition = getNextSectionPosition(mCurrentSectionPosition);
+        if (mNextSectionPosition != -1 && position == mNextSectionPosition - 1) {
+            return PINNED_HEADER_PUSHED_UP;
+        }
+
+        return PINNED_HEADER_VISIBLE;
+    }
+
+    public int getCurrentSectionPosition(int position) {
+        String listChar = mListItems.get(position).toString().substring(0, 1).toUpperCase(Locale.getDefault());
+        return mListItems.indexOf(listChar);
+    }
+
+    public int getNextSectionPosition(int currentSectionPosition) {
+        int index = mListSectionPos.indexOf(currentSectionPosition);
+        if ((index + 1) < mListSectionPos.size()) {
+            return mListSectionPos.get(index + 1);
+        }
+        return mListSectionPos.get(index);
+    }
+
+    @Override
+    public void configurePinnedHeader(View v, int position) {
+        // set text in pinned header
+        TextView header = (TextView) v;
+        mCurrentSectionPosition = getCurrentSectionPosition(position);
+        header.setText(mListItems.get(mCurrentSectionPosition));
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (view instanceof ProcedureHeaderListView) {
+            ((ProcedureHeaderListView) view).configureHeaderView(firstVisibleItem);
+        }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    public static class ViewHolder {
+        public TextView textView;
+        public CircularImageView contactPhoto;
+    }
+
+    private void getColorProceduresName( String name ) {
+        Procedures procedures = new Procedures(mContext);
+        testColor = procedures.getColorProcedureName(name);
+    }
+
+}
---
+++ app/src/main/java/ru/anroidapp/plannerwork/intface_procedure/ProcedureHeaderListView.java
@@ -0,0 +1,237 @@
+package ru.anroidapp.plannerwork.intface_procedure;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import ru.anroidapp.plannerwork.R;
+
+/*
+ * A ListView that maintains a header pinned at the top of the list. The
+ * pinned header can be pushed up and dissolved as needed.
+ *
+ */
+public class ProcedureHeaderListView extends ListView implements IIndexBarFilterProcedure {
+
+    // interface object that configure pinned header view position in list view
+    IPinnedHeaderProcedure mAdapter;
+
+    // view objects
+    View mHeaderView, mIndexBarView, mPreviewTextView;
+
+    // flags that decide view visibility
+    boolean mHeaderVisibility = false;
+    boolean mPreviewVisibility = false;
+    // initially show index bar view with it's content
+    boolean mIndexBarVisibility = true;
+
+    // context object
+    Context mContext;
+
+    // view height and width
+    int mHeaderViewWidth,
+            mHeaderViewHeight,
+            mIndexBarViewWidth,
+            mIndexBarViewHeight,
+            mIndexBarViewMargin,
+            mPreviewTextViewWidth,
+            mPreviewTextViewHeight;
+
+    // touched index bar Y axis position used to decide preview text view position
+    float mIndexBarY;
+
+
+    public ProcedureHeaderListView(Context context) {
+        super(context);
+        this.mContext = context;
+    }
+
+
+    public ProcedureHeaderListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.mContext = context;
+    }
+
+
+    public ProcedureHeaderListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.mContext = context;
+    }
+
+
+    @Override
+    public void setAdapter(ListAdapter adapter) {
+        this.mAdapter = (ProcedureHeaderAdapter) adapter;
+        super.setAdapter(adapter);
+    }
+
+
+    public void setPinnedHeaderView(View headerView) {
+        this.mHeaderView = headerView;
+        // Disable vertical fading when the pinned header is present
+        // TODO change ListView to allow separate measures for top and bottom fading edge;
+        // in this particular case we would like to disable the top, but not the bottom edge.
+        if (mHeaderView != null) {
+            setFadingEdgeLength(0);
+        }
+    }
+
+
+    public void setIndexBarView(View indexBarView) {
+        mIndexBarViewMargin = (int) mContext.getResources().getDimension(R.dimen.index_bar_view_margin);
+        this.mIndexBarView = indexBarView;
+    }
+
+
+    public void setPreviewView(View previewTextView) {
+        this.mPreviewTextView = previewTextView;
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        if (mHeaderView != null) {
+            measureChild(mHeaderView, widthMeasureSpec, heightMeasureSpec);
+            mHeaderViewWidth = mHeaderView.getMeasuredWidth();
+            mHeaderViewHeight = mHeaderView.getMeasuredHeight();
+        }
+
+        if (mIndexBarView != null && mIndexBarVisibility) {
+            measureChild(mIndexBarView, widthMeasureSpec, heightMeasureSpec);
+            mIndexBarViewWidth = mIndexBarView.getMeasuredWidth();
+            mIndexBarViewHeight = mIndexBarView.getMeasuredHeight();
+        }
+
+        if (mPreviewTextView != null && mPreviewVisibility) {
+            measureChild(mPreviewTextView, widthMeasureSpec, heightMeasureSpec);
+            mPreviewTextViewWidth = mPreviewTextView.getMeasuredWidth();
+            mPreviewTextViewHeight = mPreviewTextView.getMeasuredHeight();
+        }
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (mHeaderView != null) {
+            mHeaderView.layout(0, 0, mHeaderViewWidth, mHeaderViewHeight);
+            configureHeaderView(getFirstVisiblePosition());
+        }
+
+        if (mIndexBarView != null && mIndexBarVisibility) {
+            mIndexBarView.layout(getMeasuredWidth() - mIndexBarViewMargin - mIndexBarViewWidth, mIndexBarViewMargin
+                    , getMeasuredWidth() - mIndexBarViewMargin, getMeasuredHeight() - mIndexBarViewMargin);
+        }
+
+        if (mPreviewTextView != null && mPreviewVisibility) {
+            mPreviewTextView.layout(mIndexBarView.getLeft() - mPreviewTextViewWidth, (int) mIndexBarY - (mPreviewTextViewHeight / 2)
+                    , mIndexBarView.getLeft(), (int) (mIndexBarY - (mPreviewTextViewHeight / 2)) + mPreviewTextViewHeight);
+        }
+    }
+
+
+    public void setIndexBarVisibility(Boolean isVisible) {
+        if (isVisible) {
+            mIndexBarVisibility = true;
+        } else {
+            mIndexBarVisibility = false;
+        }
+    }
+
+
+    private void setPreviewTextVisibility(Boolean isVisible) {
+        if (isVisible) {
+            mPreviewVisibility = true;
+        } else {
+            mPreviewVisibility = false;
+        }
+    }
+
+
+    public void configureHeaderView(int position) {
+        if (mHeaderView == null) {
+            return;
+        }
+
+        int state = mAdapter.getPinnedHeaderState(position);
+
+        switch (state) {
+
+            case IPinnedHeaderProcedure.PINNED_HEADER_GONE:
+                mHeaderVisibility = false;
+                break;
+            case IPinnedHeaderProcedure.PINNED_HEADER_VISIBLE:
+                if (mHeaderView.getTop() != 0) {
+                    mHeaderView.layout(0, 0, mHeaderViewWidth, mHeaderViewHeight);
+                }
+                mAdapter.configurePinnedHeader(mHeaderView, position);
+                mHeaderVisibility = true;
+                break;
+            case IPinnedHeaderProcedure.PINNED_HEADER_PUSHED_UP:
+                View firstView = getChildAt(0);
+                int bottom = firstView.getBottom();
+                // int itemHeight = firstView.getHeight();
+                int headerHeight = mHeaderView.getHeight();
+                int y;
+                if (bottom < headerHeight) {
+                    y = (bottom - headerHeight);
+                } else {
+                    y = 0;
+                }
+
+                if (mHeaderView.getTop() != y) {
+                    mHeaderView.layout(0, y, mHeaderViewWidth, mHeaderViewHeight + y);
+                }
+                mAdapter.configurePinnedHeader(mHeaderView, position);
+                mHeaderVisibility = true;
+                break;
+        }
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);// draw list view elements (zIndex == 1)
+
+        if (mHeaderView != null && mHeaderVisibility) {
+            drawChild(canvas, mHeaderView, getDrawingTime()); // draw pinned header view (zIndex == 2)
+        }
+        if (mIndexBarView != null && mIndexBarVisibility) {
+            drawChild(canvas, mIndexBarView, getDrawingTime()); // draw index bar view (zIndex == 3)
+        }
+        if (mPreviewTextView != null && mPreviewVisibility) {
+            drawChild(canvas, mPreviewTextView, getDrawingTime()); // draw preview text view (zIndex == 4)
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (mIndexBarView != null && (mIndexBarView).onTouchEvent(ev)) {
+            setPreviewTextVisibility(true);
+            return true;
+        } else {
+            setPreviewTextVisibility(false);
+            return super.onTouchEvent(ev);
+        }
+    }
+
+
+    @Override
+    public void filterList(float indexBarY, int position, String previewText) {
+        this.mIndexBarY = indexBarY;
+
+        if (mPreviewTextView instanceof TextView)
+            ((TextView) mPreviewTextView).setText(previewText);
+
+        setSelection(position);
+    }
+}
+

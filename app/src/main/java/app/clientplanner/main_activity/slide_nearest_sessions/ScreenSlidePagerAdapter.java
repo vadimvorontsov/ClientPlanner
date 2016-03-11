446335e741bacb222873e97fa1fbc151701ec350
Vadim Vorontsov <voronczov-vadim@mail.ru>
2016-03-11 19:01:06 +0300

    v_11.03_19:10

--- app/src/main/java/app/clientplanner/main_activity/slide_nearest_sessions/ScreenSlidePagerAdapter.java
+++ app/src/main/java/app/clientplanner/main_activity/slide_nearest_sessions/ScreenSlidePagerAdapter.java
@@ -11,15 +11,11 @@ public class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
 
     private Context mContext;
     private ArrayList<Long> mSessions;
-//    private Map<Integer, String> mFragmentTags;
-//    private FragmentManager mFM;
 
     public ScreenSlidePagerAdapter(Context ctx, FragmentManager fm, ArrayList<Long> sessions) {
         super(fm);
         this.mContext = ctx;
-//        this.mFM = fm;
         mSessions = sessions;
-//        mFragmentTags = new HashMap<>();
     }
 
     @Override
